6c738f638fd2b672360c689cdec9e4444cf8b9b6
联盟少侠 <1121114685@qq.com>
2020-03-16 12:04:34 +0800

    功能性更新和完善

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
 	"time"
+	"unicode/utf8"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/gogf/gf/text/gregex"
@@ -18,6 +20,7 @@ func main() {
 	newExcel.SetCellValue("Sheet1", "B1", "OptionList") // 创建首行db关键字 选项
 	newExcel.SetCellValue("Sheet1", "C1", "Result")     // 创建首行db关键字 答案
 	newExcel.SetCellValue("Sheet1", "D1", "Explain")    // 创建首行db关键字 解析
+	newExcel.SetCellValue("Sheet1", "E1", "Type")       // 创建首行db关键字 题型 1 判断  2 单选 3 多选 4 填空 5 简答
 	// 逐行扫描txt，将他们写入excel
 	file, err := os.Open("./导入文本.txt")
 	if err != nil {
@@ -26,6 +29,8 @@ func main() {
 	defer file.Close()
 
 	LineExcel := 2 // 给excel 赋值正在使用的行号赋值
+	Answer := ""   // 简答题/选择题 答案 变量
+
 	Scanner := bufio.NewScanner(file)
 	for Scanner.Scan() {
 
@@ -33,6 +38,7 @@ func main() {
 		Txts = Scanner.Text()
 
 		for strings.Contains(Txts, "[填空题]") { // 如果字符串里面包含了[填空题]，就进行下面的
+			Answer = "" // 将答案清空，避免其他问题导致答案错误
 
 			for Scanner.Scan() {
 
@@ -51,34 +57,62 @@ func main() {
 					break // 退出循环
 
 				}
+				if strings.Contains(Txts, "[填空题]") { // 包含关键字 退出循环
+					break // 退出循环
 
-				LineExcelString := strconv.Itoa(LineExcel)
-				newExcel.SetCellValue("Sheet1", "A"+LineExcelString, Txts)
+				}
+
+				LineExcelString := strconv.Itoa(LineExcel) // 数字类型转字符串类型
+				// 提取题目中的答案
+				match, _ := gregex.MatchString(`\(\(.*\)\)`, Txts) // 正则表达式中的* 表示前一个字符出现任意次，与我们所谓的*匹配任意字符不同  参见 https://zh.wikipedia.org/wiki/%E6%AD%A3%E5%88%99%E8%A1%A8%E8%BE%BE%E5%BC%8F
+				if match != nil {
+					result := match[len(match)-1]
+					// 排除((  )) 只提取里面的内容
+					//resultReg, err := gregex.MatchString(`[^\(\)]`, result) // 排除型字符集合（negated character classes）。匹配未列出的任意字符。例如，“[^abc]”可以匹配“plain”中的“plin”。
+					Answer, err = gregex.ReplaceString(`\(|\)`, "", result) // 参数含义 需要替换的字符  替换后的字符 目标处理文件
+					if err != nil {
+						println(err.Error())
+					}
+
+					newExcel.SetCellValue("Sheet1", "B"+LineExcelString, Answer) // 写入填空题答案
+					Txts, err = gregex.ReplaceString(result, " ", Txts)          // 参数含义 需要替换的字符  替换后的字符 目标处理文件
+					if err != nil {
+						println(err.Error())
+					}
+					println("本题填空题 答案：" + LineExcelString + " 行 " + Answer)
+				} else {
+					newExcel.SetCellValue("Sheet1", "B"+LineExcelString, "本题答案为空")
+
+				}
+
+				newExcel.SetCellValue("Sheet1", "A"+LineExcelString, Txts) // 题目
+				newExcel.SetCellValue("Sheet1", "E"+LineExcelString, "4")  //题目类型
+				println("填空题在ECXEL的：" + LineExcelString + " 行 " + Txts)
+
+				// 录入解析
+				if strings.Contains(Txts, "[解析]") { // 包含关键字 退出循环
+					newExcel.SetCellValue("Sheet1", "D"+LineExcelString, Txts)
+					println("解析  在ECXEL的：" + LineExcelString + " 行 " + Txts)
+
+				}
 				LineExcel++
 
-				println(Txts)
 			}
 		}
 
-		var TxtsA string // 多选 单选 选项A
-		var TxtsB string
-		var TxtsC string
-		var TxtsD string
-
 		for strings.Contains(Txts, "[选择题]") { // 如果字符串里面包含了[填空题]，就进行下面的
+			Answer = ""
 			for Scanner.Scan() {
 
 				Txts = Scanner.Text()
 
 				LineExcelString := strconv.Itoa(LineExcel)
-				// var TxtsA string
-				// var TxtsB string
-				// var TxtsC string
-				// var TxtsD string
-				if strings.Contains(Txts, "[填空题]") { // 包含关键字 退出循环
+
+				if strings.Contains(Txts, "[选择题]") { // 包含关键字 退出循环
 					break // 退出循环
 
 				}
+
 				if strings.Contains(Txts, "[判断题]") { // 包含关键字 退出循环
 					break // 退出循环
 
@@ -87,56 +121,105 @@ func main() {
 					break // 退出循环
 
 				}
+				if strings.Contains(Txts, "[填空题]") { // 包含关键字 退出循环
+					break // 退出循环
+
+				}
 
 				if strings.Contains(Txts, "A.") && strings.Contains(Txts, "B.") && strings.Contains(Txts, "C.") && strings.Contains(Txts, "D.") { // 如果字段包含A B C D 就当成是选项
-					if strings.Contains(Txts, "ABCD") {
-						newExcel.SetCellValue("Sheet1", "A"+LineExcelString, Txts)
-						LineExcel--
-					}
+
 					Txts = strings.Replace(Txts, " B.", ";;B.", -1)
 					Txts = strings.Replace(Txts, " C.", ";;C.", -1)
 					Txts = strings.Replace(Txts, " D.", ";;D.", -1) // 替换字符串
+					Txts = strings.Replace(Txts, " E.", ";;E.", -1) // 替换字符串
+					Txts = strings.Replace(Txts, " F.", ";;F.", -1) // 替换字符串
+					Txts = strings.Replace(Txts, " G.", ";;G.", -1) // 替换字符串
 					newExcel.SetCellValue("Sheet1", "B"+LineExcelString, Txts)
+					println("选项   在ECXEL的：" + LineExcelString + " 行 " + Txts)
 					LineExcel++
-					println(Txts)
+
 				} else if strings.Contains(Txts, "A.") { // 如果包含A.
+					if Answer == "" {
+						Answer = Txts
+					} else {
+						Answer = Answer + ";;" + Txts // 将结果存入Answer中
+					}
 
-					TxtsA = Txts // 将结果存入txtsA中
+					// Txts = Scanner.Text()
+					// println("选择题在ECXEL的：" + LineExcelString + " 行 " + Txts)
 
-					Txts = Scanner.Text()
-					println(Txts)
 				} else if strings.Contains(Txts, "B.") { // 如果包含B.
-					TxtsB = Txts
+					if Answer == "" {
+						Answer = Txts
+					} else {
+						Answer = Answer + ";;" + Txts // 将结果存入Answer中
+					}
 
-					Txts = Scanner.Text()
-					println(Txts)
 				} else if strings.Contains(Txts, "C.") { // 如果包含B.
-					TxtsC = Txts
+					if Answer == "" {
+						Answer = Txts
+					} else {
+						Answer = Answer + ";;" + Txts // 将结果存入Answer中
+					}
 
-					Txts = Scanner.Text()
-					println(Txts)
 				} else if strings.Contains(Txts, "D.") { // 如果包含B.
-					TxtsD = Txts
-					newExcel.SetCellValue("Sheet1", "B"+LineExcelString, TxtsA+";;"+TxtsB+";;"+TxtsC+";;"+TxtsD)
+					if Answer == "" {
+						Answer = Txts
+					} else {
+						Answer = Answer + ";;" + Txts // 将结果存入Answer中
+					}
+					newExcel.SetCellValue("Sheet1", "B"+LineExcelString, Answer)
+					println("选项  在ECXEL的：" + LineExcelString + " 行 " + Answer)
+				} else if strings.Contains(Txts, "E.") { // 如果包含B.
+
+					Answer = Answer + ";;" + Txts // 将结果存入Answer中
+					newExcel.SetCellValue("Sheet1", "B"+LineExcelString, Answer)
+					println("选项  在ECXEL的：" + LineExcelString + " 行 " + Answer)
+				} else if strings.Contains(Txts, "F.") { // 如果包含B.
+
+					Answer = Answer + ";;" + Txts // 将结果存入Answer中
+					newExcel.SetCellValue("Sheet1", "B"+LineExcelString, Answer)
+					println("选项  在ECXEL的：" + LineExcelString + " 行 " + Answer)
+				} else if strings.Contains(Txts, "G.") { // 如果包含B.
+
+					Answer = Answer + ";;" + Txts // 将结果存入Answer中
+					newExcel.SetCellValue("Sheet1", "B"+LineExcelString, Answer)
+					println("选项  在ECXEL的：" + LineExcelString + " 行 " + Answer)
 					LineExcel++
 				} else {
 
-					// var ResultReg *regexp.Regexp
-					match, _ := gregex.MatchString(`\(.*[A-D]+.*\)`, Txts) // 正则表达式中的* 表示前一个字符出现任意次，与我们所谓的*匹配任意字符不同  参见 https://zh.wikipedia.org/wiki/%E6%AD%A3%E5%88%99%E8%A1%A8%E8%BE%BE%E5%BC%8F
+					// 提取题目中的答案
+					match, _ := gregex.MatchString(`\(.*[A-G]+.*\)`, Txts) // 正则表达式中的* 表示前一个字符出现任意次，与我们所谓的*匹配任意字符不同  参见 https://zh.wikipedia.org/wiki/%E6%AD%A3%E5%88%99%E8%A1%A8%E8%BE%BE%E5%BC%8F
 					if match != nil {
 						result := match[len(match)-1]
 
-						// Result := reg.FindAllString(Txts, -1)
-						// ResultReg = regexp.MustCompile(`(\(|\ |\))`) // 查找 括号 空格 反括弧
-						// Result = ResultReg.ReplaceAllString(result_1, "")
-						resultReg, _ := gregex.MatchString(`[A-D]+`, result)
-						newExcel.SetCellValue("Sheet1", "C"+LineExcelString, resultReg[0]) // 写入选择题答案
-						Txts, err = gregex.ReplaceString(result, " ", Txts)                // 参数含义 需要替换的字符  替换后的字符 目标处理文件
+						resultReg, err := gregex.MatchString(`[A-G]+`, result)
 						if err != nil {
 							println(err.Error())
 						}
+						newExcel.SetCellValue("Sheet1", "C"+LineExcelString, resultReg[len(resultReg)-1]) // 写入选择题答案
+						Txts, err = gregex.ReplaceString(result, " ", Txts)                               // 参数含义 需要替换的字符  替换后的字符 目标处理文件
+						if err != nil {
+							println(err.Error())
+						}
+						if utf8.RuneCountInString(resultReg[len(resultReg)-1]) > 1 { // 答案长度超过1 则为多选
+							newExcel.SetCellValue("Sheet1", "E"+LineExcelString, "3") //题目类型  多选
+						} else {
+
+							newExcel.SetCellValue("Sheet1", "E"+LineExcelString, "2") //题目类型  单选
+
+						}
+						println("本题选择题 答案：" + LineExcelString + " 行 " + resultReg[len(resultReg)-1])
 					}
 					newExcel.SetCellValue("Sheet1", "A"+LineExcelString, Txts) // 题目不换行
+					println("选择题在ECXEL的：" + LineExcelString + " 行 " + Txts)
+
+					// 录入解析
+					if strings.Contains(Txts, "[解析]") { // 包含关键字 退出循环
+						newExcel.SetCellValue("Sheet1", "D"+LineExcelString, Txts)
+						println("解析  在ECXEL的：" + LineExcelString + " 行 " + Txts)
+
+					}
 
 				}
 			}
@@ -161,7 +244,10 @@ func main() {
 					break // 退出循环
 
 				}
+				if strings.Contains(Txts, "[填空题]") { // 包含关键字 退出循环
+					break // 退出循环
 
+				}
 				LineExcelString := strconv.Itoa(LineExcel)
 				if strings.Contains(Txts, "√") {
 					newExcel.SetCellValue("Sheet1", "C"+LineExcelString, "Y")
@@ -174,15 +260,68 @@ func main() {
 					println(err.Error())
 				}
 				newExcel.SetCellValue("Sheet1", "A"+LineExcelString, Txts)
+				newExcel.SetCellValue("Sheet1", "E"+LineExcelString, "1") //题目类型 判断
+				println("判断题在ECXEL的：" + LineExcelString + " 行 " + Txts)
+
+				// 录入解析
+				if strings.Contains(Txts, "[解析]") { // 包含关键字 退出循环
+					newExcel.SetCellValue("Sheet1", "D"+LineExcelString, Txts)
+					println("解析  在ECXEL的：" + LineExcelString + " 行 " + Txts)
+
+				}
+
 				LineExcel++
 
-				println(Txts)
 			}
 		}
-		//println(Scanner.Text())
-		// fmt.Printf("值类型为 :%T\n", Scanner.Text())
-		// println(Scanner.Text())
-		//	lines = append(lines, Scanner.Text()) // append函数是用来在slice末尾追加一个或者多个元素。
+
+		for strings.Contains(Txts, "[简答题]") { // 如果字符串里面包含了[填空题]，就进行下面的
+			Answer = ""
+
+			for Scanner.Scan() {
+
+				Txts = Scanner.Text()
+				if strings.Contains(Txts, "[选择题]") { // 包含关键字 退出循环
+					break // 退出循环
+
+				}
+
+				if strings.Contains(Txts, "[判断题]") { // 包含关键字 退出循环
+					break // 退出循环
+
+				}
+				if strings.Contains(Txts, "[简答题]") { // 包含关键字 退出循环
+					break // 退出循环
+
+				}
+				if strings.Contains(Txts, "[填空题]") { // 包含关键字 退出循环
+					break // 退出循环
+
+				}
+
+				LineExcelString := strconv.Itoa(LineExcel)
+
+				if strings.Contains(Txts, "[题目]") {
+					if Answer != "" { //
+						newExcel.SetCellValue("Sheet1", "D"+LineExcelString, Answer)
+						println("答案   在ECXEL的：" + LineExcelString + " 行 " + Answer)
+						Answer = ""
+					}
+					Txts, err = gregex.ReplaceString(`\[题目\]`, " ", Txts) // 参数含义 需要替换的字符  替换后的字符 目标处理文件
+					if err != nil {
+						println(err.Error())
+					}
+					newExcel.SetCellValue("Sheet1", "A"+LineExcelString, Txts)
+					newExcel.SetCellValue("Sheet1", "E"+LineExcelString, "5") //题目类型 简答
+					println("简答题在ECXEL的：" + LineExcelString + " 行 " + Txts)
+					LineExcel++
+				} else {
+					Answer = Answer + "\n" + Txts
+
+				}
+
+			}
+		}
 
 	}
 
@@ -192,4 +331,10 @@ func main() {
 	if err := newExcel.SaveAs("联盟少侠" + t.Format("20060102150405") + ".xlsx"); err != nil {
 		println(err.Error())
 	}
+	// 等待用户关闭（输入）
+	println("软件开源地址：https://github.com/user1121114685/tiku")
+	println("执行完毕........请关闭窗口......")
+	var exitScan string
+	_, _ = fmt.Scan(&exitScan)
+
 }
